2940ca28a116174a1795438168332fb68fa76775
Abderrhmane Ch <abderrhmanechanderli22@gmail.com>
2025-08-25 15:24:38 +0100

    Update test_silver.sql

--- tests/test_silver.sql
+++ tests/test_silver.sql
@@ -1,4 +1,24 @@
 /*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Data Normalization.
+    - Invalid date ranges and orders.
+    - Data consistency between related fields.
+
+Usage Notes:
+    - Run these checks after data loading Silver Layer.
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+/*
 ============================================================
 Check For Table  : olist_customers
 ============================================================
