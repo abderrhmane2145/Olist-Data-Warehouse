3fdbb9b55def2dcf116cf635d493205e568ef20f
Abderrhmane Ch <abderrhmanechanderli22@gmail.com>
2025-08-27 20:42:35 +0100

    Update dll_silver.sql

--- scripts/silver/dll_silver.sql
+++ scripts/silver/dll_silver.sql
@@ -15,10 +15,10 @@ IF OBJECT_ID('silver.olist_customers','U') IS NOT NULL
    DROP TABLE silver.olist_customers;
 GO
 CREATE TABLE silver.olist_customers (
-	customer_id              NVARCHAR(200),
-	customer_zip_code_prefix NVARCHAR(50),
-	customer_city            NVARCHAR(200),
-	customer_state           NVARCHAR(55),
+	customer_id              NVARCHAR(35),
+	customer_zip_code_prefix INT,
+	customer_city            NVARCHAR(35),
+	customer_state           NVARCHAR(35),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -29,8 +29,8 @@ CREATE TABLE silver.olist_geolocation (
 	geolocation_zip_code_prefix INT,
 	geolocation_lat             FLOAT,
 	geolocation_lng             FLOAT,
-	geolocation_city            NVARCHAR(200),
-	geolocation_state           NVARCHAR(200),
+	geolocation_city            NVARCHAR(35),
+	geolocation_state           NVARCHAR(35),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -38,10 +38,10 @@ IF OBJECT_ID('silver.olist_order_items','U') IS NOT NULL
    DROP TABLE silver.olist_order_items;
 GO
 CREATE TABLE  silver.olist_order_items (
-	order_id             NVARCHAR(200),
+	order_id             NVARCHAR(35),
 	order_item_id        INT,
-	product_id           NVARCHAR(200),
-	seller_id            NVARCHAR(200),
+	product_id           NVARCHAR(35),
+	seller_id            NVARCHAR(35),
 	shipping_limit_date  DATETIME2,
 	price                FLOAT,
 	freight_value        FLOAT,
@@ -54,9 +54,9 @@ IF OBJECT_ID('silver.olist_order_payments','U') IS NOT NULL
    DROP TABLE silver.olist_order_payments;
 GO
 CREATE TABLE silver.olist_order_payments (
-	order_id              NVARCHAR(200),
+	order_id              NVARCHAR(35),
 	payment_sequential    INT,
-	payment_type          NVARCHAR(200),
+	payment_type          NVARCHAR(35),
 	payment_installments  INT,
 	payment_value         FLOAT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
@@ -66,11 +66,11 @@ IF OBJECT_ID('silver.olist_order_reviews','U') IS NOT NULL
    DROP TABLE silver.olist_order_reviews;
 GO
 CREATE TABLE silver.olist_order_reviews (
-	review_id              NVARCHAR(200),
-	order_id               NVARCHAR(200),
+	review_id              NVARCHAR(35),
+	order_id               NVARCHAR(35),
 	review_score           INT ,
-	review_comment_title   NVARCHAR(500),
-	review_comment_message NVARCHAR(500),
+	review_comment_title   NVARCHAR(70),
+	review_comment_message NVARCHAR(300),
 	review_creation_date   DATETIME2,
 	review_answer_timestamp DATETIME2,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
@@ -80,9 +80,9 @@ IF OBJECT_ID('silver.olist_orders','U') IS NOT NULL
    DROP TABLE silver.olist_orders;
 GO
 CREATE TABLE silver.olist_orders (
-	order_id NVARCHAR(200),
-	customer_id NVARCHAR(200),
-	order_status NVARCHAR(200),
+	order_id NVARCHAR(35),
+	customer_id NVARCHAR(35),
+	order_status NVARCHAR(35),
 	order_purchase_timestamp DATETIME2, 
 	order_approved_at   DATETIME2,
 	order_delivered_carrier_date DATETIME2,
@@ -96,8 +96,8 @@ IF OBJECT_ID('silver.olist_products','U') IS NOT NULL
    DROP TABLE silver.olist_products;
 GO
 CREATE TABLE silver.olist_products (
-	product_id              NVARCHAR(200),
-	product_category_name   NVARCHAR(200),
+	product_id              NVARCHAR(35),
+	product_category_name   NVARCHAR(50),
 	product_name_length     FLOAT,
 	product_photos_qty  FLOAT,
 	product_weight_g    FLOAT,
@@ -112,10 +112,10 @@ IF OBJECT_ID('silver.olist_sellers','U') IS NOT NULL
    DROP TABLE silver.olist_sellers;
 GO
 CREATE TABLE silver.olist_sellers (
-	seller_id NVARCHAR(200),
+	seller_id NVARCHAR(35),
 	seller_zip_code_prefix INT,
-	seller_city NVARCHAR(200),
-	seller_state NVARCHAR(200),
+	seller_city NVARCHAR(35),
+	seller_state NVARCHAR(35),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -123,8 +123,8 @@ IF OBJECT_ID('silver.olist_translation_category','U') IS NOT NULL
    DROP TABLE silver.olist_translation_category;
 GO
 CREATE TABLE silver.olist_translation_category (
-	product_category_name NVARCHAR(200),
-	product_category_name_english NVARCHAR(200),
+	product_category_name NVARCHAR(50),
+	product_category_name_english NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
