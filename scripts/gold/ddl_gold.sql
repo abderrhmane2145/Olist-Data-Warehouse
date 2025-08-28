4bba575da590fe9813e9176d975e092cd508041c
Abderrhmane Ch <abderrhmanechanderli22@gmail.com>
2025-08-28 17:59:54 +0100

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -25,13 +25,13 @@ CREATE VIEW gold.dim_product AS
 (
 SELECT 
     ROW_NUMBER() OVER(ORDER BY p.product_id) AS product_key, -- Surrogate Key
-  	p.product_id,
-  	t.product_category_name_english AS category_english,
-    p.product_category_name AS category_portuguese,
-    p.product_name_length AS name_length,
-  	COALESCE(p.product_photos_qty, 0) AS photo_number,
-  	p.product_weight_g AS weight_g,
-  	p.product_volume_cm3 AS volume_cm3
+	p.product_id,
+	t.product_category_name_english AS category_english,
+	p.product_category_name AS category_portuguese,
+	p.product_name_length AS name_length,
+	COALESCE(p.product_photos_qty, 0) AS photo_number,
+	p.product_weight_g AS weight_g,
+	p.product_volume_cm3 AS volume_cm3
 FROM silver.olist_products p
 LEFT JOIN silver.olist_translation_category t
 ON p.product_category_name = t.product_category_name
@@ -51,13 +51,13 @@ GO
 CREATE VIEW gold.dim_seller AS 
 (
 SELECT 
-  	ROW_NUMBER() OVER(ORDER BY seller_id) AS seller_key, -- Surrogate Key
-  	s.seller_id,
-  	s.seller_city,
-  	s.seller_state,
-  	s.seller_zip_code_prefix,
-  	g.latitude,
-  	g.longitude
+	ROW_NUMBER() OVER(ORDER BY seller_id) AS seller_key, -- Surrogate Key
+	s.seller_id,
+	s.seller_city,
+	s.seller_state,
+	s.seller_zip_code_prefix,
+	g.latitude,
+	g.longitude
 FROM silver.olist_sellers s
 LEFT JOIN 
 	(
@@ -71,8 +71,8 @@ LEFT JOIN
 	WHERE geolocation_lat IS NOT NULL AND geolocation_lng IS NOT NULL
 	GROUP BY  geolocation_zip_code_prefix, geolocation_city, geolocation_state
 	) g
-ON  LOWER(TRIM(s.seller_state)) = LOWER(TRIM(g.geolocation_state))
-AND LOWER(TRIM(s.seller_city)) = LOWER(TRIM(g.geolocation_city))
+ON  s.seller_state = g.geolocation_state
+AND s.seller_city   = g.geolocation_city
 AND s.seller_zip_code_prefix = g.geolocation_zip_code_prefix
 WHERE s.seller_id IS NOT NULL 
   AND s.seller_city IS NOT NULL 
@@ -93,22 +93,22 @@ GO
 CREATE VIEW gold.dim_customer AS 
 (
 SELECT 
-    	ROW_NUMBER() OVER(ORDER BY customer_id) AS customer_key, -- Surrogate key
-    	c.customer_id,
-    	c.customer_city,
-    	c.customer_state,
-    	c.customer_zip_code_prefix,
-    	g.latitude,
-    	g.longitude
+	ROW_NUMBER() OVER(ORDER BY customer_id) AS customer_key, -- Surrogate key
+	c.customer_id,
+	c.customer_city,
+	c.customer_state,
+	c.customer_zip_code_prefix,
+	g.latitude,
+	g.longitude
 FROM silver.olist_customers c
 LEFT JOIN 
 	(
 	SELECT 
-  		geolocation_zip_code_prefix,
-  		geolocation_city,
-  		geolocation_state,
-  		AVG(geolocation_lat) AS latitude,
-  		AVG(geolocation_lng) AS longitude
+		geolocation_zip_code_prefix,
+		geolocation_city,
+		geolocation_state,
+		AVG(geolocation_lat) AS latitude,
+		AVG(geolocation_lng) AS longitude
 	FROM silver.olist_geolocation
 	GROUP BY  geolocation_zip_code_prefix, geolocation_city, geolocation_state
 	) g
@@ -133,28 +133,49 @@ GO
 CREATE VIEW gold.fact_orders AS 
 (
 SELECT 
-  	o.order_id,
-  	c.customer_key,
-  	p.product_key,
-  	s.seller_key,
-  	o.order_status,
-  	oi.price,
-  	oi.freight_value,
-  	oi.total_price,
-	oi.shipping_limit_date AS shipping_date,
-  	DATEDIFF(day, o.order_purchase_timestamp, o.order_delivered_customer_date) AS duration_sell_day,
-  	DATEDIFF(day, o.order_delivered_carrier_date, o.order_delivered_customer_date) AS duration_delivery_day,
-  	op.payment_sequential,
-  	op.payment_type,
-  	op.payment_installments,
-  	op.payment_value,
-  	re.review_score
+	o.order_id,
+	c.customer_key,
+	p.product_key,
+	s.seller_key,
+	o.order_status,
+	oi.price,
+	oi.freight_value,
+	oi.total_price,
+	oi.shipping_date,
+	DATEDIFF(day, o.order_purchase_timestamp, o.order_delivered_customer_date) AS duration_sell_day,
+	DATEDIFF(day, o.order_delivered_carrier_date, o.order_delivered_customer_date) AS duration_delivery_day,
+	op.payment_sequential,
+	op.payment_type,
+	op.payment_installments,
+	op.payment_value,
+	re.review_score
 FROM silver.olist_orders o
 	LEFT JOIN silver.olist_order_reviews re
 	   ON o.order_id = re.order_id
-	LEFT JOIN silver.olist_order_payments op
+	LEFT JOIN 
+			(SELECT 
+			   order_id,
+			   SUM(payment_value) AS payment_value,
+			   MAX(payment_installments) AS payment_installments,
+			   MAX(payment_sequential) AS payment_sequential,
+			   (SELECT TOP 1 payment_type 
+					 FROM silver.olist_order_payments p2 
+					 WHERE p2.order_id = p1.order_id 
+					 ORDER BY payment_value DESC) AS payment_type
+			 FROM silver.olist_order_payments p1
+			 GROUP BY order_id)  op 
 	   ON o.order_id = op.order_id
-	LEFT JOIN silver.olist_order_items oi
+	LEFT JOIN 
+				(SELECT
+					  order_id,
+					  product_id,
+					  seller_id,
+					  SUM(price) AS price,
+					  SUM(freight_value) AS freight_value,
+					  SUM(price + freight_value) AS total_price,
+					  MAX(shipping_limit_date) AS shipping_date
+				 FROM silver.olist_order_items
+				 GROUP BY order_id, product_id, seller_id) oi 
 	   ON o.order_id = oi.order_id
 	LEFT JOIN gold.dim_customer c
 	   ON o.customer_id = c.customer_id
@@ -163,3 +184,4 @@ FROM silver.olist_orders o
 	LEFT JOIN gold.dim_seller s
 	   ON oi.seller_id = s.seller_id
 );
+
